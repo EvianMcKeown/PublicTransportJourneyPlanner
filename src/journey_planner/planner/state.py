298983f97e59e30667722217a400e52582af63a7
Evian McKeown <mckevi001@cs.uct.ac.za>
2025-08-19 19:32:24 +0200

    fixed typo

--- src/journey_planner/planner/state.py
+++ src/journey_planner/planner/state.py
@@ -1,5 +1,5 @@
 # File for the states of the planner app
-# ie. 'Awaiting Input', 'Calculating Route', 'Shwoing Route', Error', etc.
+# ie. 'Awaiting Input', 'Calculating Route', 'Showing Route', Error', etc.
 #
 # This will determine the current condition of the request and what action should be taken based on the current sate.
 # --- CONTROL LOGIC ---
